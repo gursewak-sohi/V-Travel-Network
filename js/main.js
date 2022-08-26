2f816e372ebf226970b191dbbe8bca76f28ce74f
gursewak <gursewak@gursewaks-MacBook-Pro.local>
2022-08-26 14:23:23 +0530

    header + effects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
- 
+const header = document.querySelector(".header");
+if (header) {
+    // Animate Header on scroll
+    let scrollpos = window.scrollY
+    const header_height = header.offsetHeight
+    const addClassOnScroll = () => header.classList.add("header-active")
+    const remClassOnScroll = () => header.classList.remove("header-active")
+    window.addEventListener('scroll', function() {
+        scrollpos = window.scrollY;
+        if (scrollpos >= 50) { addClassOnScroll() } else { remClassOnScroll() }
+    })
+}
